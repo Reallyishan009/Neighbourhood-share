aafc1352800a07772ca299b96a80d930fd393095
Reallyishan009 <ishantripathidoc@gmail.com>
2025-08-03 16:07:10 +0530

    baseurl

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const apiClient = axios.create({
-     baseURL : "https://neighbourhood-share-eight.vercel/api",
+     baseURL : "https://neighbourhood-share-eight.vercel.app/api",
      timeout : 10000,
      headers : {
         'Content-Type' : 'application/json',
